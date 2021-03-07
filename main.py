5aa0936f6c7a671e6f6887769b2e5b5ea6594765
Adrian <adriansam.pl3@gmail.com>
2021-03-07 23:11:49 +0100

    zad

--- main.py
+++ main.py
@@ -10,3 +10,17 @@
 # lista.append('Star wars last jedi')
 # lista.append('The thing')
 # print(lista)
+# # zad2
+# slownik={'top1':lista[0], 'top2':lista[1], 'top3':lista[2],
+#          'top4':lista[3], 'top5':lista[4], 'top6':lista[5],
+#          'top7':lista[6], 'top8':lista[7], 'top9':lista[8],
+#          'top10':lista[9]}
+# print(slownik['top1'])
+# zad3
+# slownik3={1:'CAD-komputerowe wspomaganie projektowania', 2:'Wizualizajca danych', 3:'J.angielski', 4:'Prog. strukt.', 5:'El. mat. dys.', 6:'Rachunek rozn.'}
+# x=len(slownik3)
+# print(x)
+# zad4
+
+
+
