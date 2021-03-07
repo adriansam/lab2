d776624d90ebc3a5606019afa08f20654d025693
Adrian <adriansam.pl3@gmail.com>
2021-03-07 20:14:47 +0100

    zad

---
+++ main.py
@@ -0,0 +1,12 @@
+#zad1
+# lista = ['Pustkowie Smauga', 'Niezwykla podroz', 'Bitwa pieciu armii', 'Dwie wieze']
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.insert(5,'Druzyna pierscienia')
+# lista.append('Interstellar')
+# lista.append('Theory of Everything')
+# lista.append('Star wars jedi awaken')
+# lista.append('Star wars last jedi')
+# lista.append('The thing')
+# print(lista)
